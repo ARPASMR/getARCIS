730a94c7d0db831dd037fc1cd56e87dbeb179f37
msalvati <m.salvati@arpalombardia.it>
2021-02-01 11:40:33 +0100

    inserito while -sleep per riprovare

--- download_arcis.sh
+++ download_arcis.sh
@@ -10,11 +10,8 @@
 
 # date
 anno_inizio=$(date --date="last month" +%Y)
-#echo $anno_inizio
 mese_inizio=$(date --date="last month" +%m)
-#echo $mese_inizio
-giorno_inizio=$anno_inizio-$mese_inizio-01
-#echo $giorno_inizio
+
 echo "Chiedo i dati per: anno " $anno_inizio ", mese: " $mese_inizio
 
 echo " ------------------------------------------------ "
@@ -24,6 +21,10 @@ echo " ------------------------------------------------ "
 echo " ------------------------------------------------ "
 echo " Scarico dataset arcis: ARCIS_GG_"$anno_inizio""$mese_inizio".nc"
 echo " ------------------------------------------------ "
+
+
+while [! -f "DATASET/ARCIS_GG_"$anno_inizio""$mese_inizio".nc" ]
+do 
 
 echo " ----  ftp arpa piemonte -------"
 $FTP -n -v -d $ARCIS_FTP <<FINE2 > $LOGFILE 2>&1
@@ -36,13 +37,9 @@ $FTP -n -v -d $ARCIS_FTP <<FINE2 > $LOGFILE 2>&1
     bye
 FINE2
 
-#   inserisco chiamata syslogger
-#   if [[ "$?" -ge "300" ]]; then
-#      logger -i -s -p user.err "Problema nel download dataset ARCIS $mese_inizio/$anno_inizio " -t "DATI"
-#    else
-#      logger -i -s -p user.notice "Trasferimento dataset $mese_inizio/$anno_inizio da ftp ARCIS  completato" -t "DATI"
-#    fi
-#   fine chiamata syslogger
+sleep 1d
+done
+
 
 /usr/bin/smbclient //$WEBPREVIP/$WEBPREVDIR -U $WEBPREVUSR%$WEBPREVPWD <<End-of-smbclient8
 prompt
